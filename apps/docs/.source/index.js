0de7a9744e5830bdfffe6ea769cf4aeb322cdc98
SOG-web <raheemolalekanusman84@gmail.com>
2025-01-27 17:26:21 +0100

    added docs

---
+++ apps/docs/.source/index.js
@@ -0,0 +1,5 @@
+import { toRuntime, toRuntimeAsync } from "fumadocs-mdx"
+import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=e2a97abc94d55e5c23fc69e0862f6edf"
+import * as docs_1 from "../content/docs/(auth-methods)/auth-methods.mdx?collection=docs&hash=e2a97abc94d55e5c23fc69e0862f6edf"
+export const docs = [toRuntime("doc", docs_0, {"path":"index.mdx","absolutePath":"/Users/rou/Desktop/bass/forgebase-ts/apps/docs/content/docs/index.mdx"}), toRuntime("doc", docs_1, {"path":"(auth-methods)/auth-methods.mdx","absolutePath":"/Users/rou/Desktop/bass/forgebase-ts/apps/docs/content/docs/(auth-methods)/auth-methods.mdx"})];
+export const meta = [];
