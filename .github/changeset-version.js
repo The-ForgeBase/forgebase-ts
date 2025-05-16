b02ef87f079d753153d321fb76c80390c975d42d
SOG-web <raheemolalekanusman84@gmail.com>
2025-05-16 19:45:12 +0100

    feat(auth): add ESM and CJS dual package support

--- .github/changeset-version.js
+++ .github/changeset-version.js
@@ -1,3 +1,9 @@
 const { exec } = require('child_process');
 exec('pnpx changeset version');
 exec('pnpm install');
+exec('pnpm turbo run build --filter=./packages/common');
+exec('pnpm turbo run build --filter=./packages/database');
+exec('pnpm turbo run build --filter=./packages/auth');
+exec('pnpm turbo run build --filter=./packages/api');
+exec('pnpm turbo run build --filter=./packages/storage');
+exec('pnpm turbo run build --filter=./packages/sdk');
