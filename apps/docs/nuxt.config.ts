2a098e7c6529ffd2ac04a604fdb8e2e4d377f499
SOG-web <raheemolalekanusman84@gmail.com>
2025-05-18 01:30:16 +0100

    added docs and studio

---
+++ apps/docs/nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  extends: ['shadcn-docs-nuxt'],
+  compatibilityDate: '2024-07-06',
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    // '@nuxt/ui',
+  ],
+});
